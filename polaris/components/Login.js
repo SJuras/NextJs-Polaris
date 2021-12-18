90a0d2f0584ca22f915bb26aea41bd196ce2f57f
SJuras <jurasstpko@gmailcom>
2021-12-18 11:06:42 +0100

    v1.1.2 basic login screen with testing images added.

---
+++ polaris/components/Login.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+function Login(){
+  return(
+    <div className="bg-black relative text-white">
+      <h1>Login Screen</h1>
+      <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
+        {/* logo */}
+        <Image
+          className=""
+          src="/images/logo1.png"
+          height={200}
+          width={400}
+        />
+        {/* login button */}
+        <button className="bg-blue-500 p-5 font-bold animate-pulse">Login to the MetaVerse</button>
+      </div>
+
+      <div className="w-full h-screen">
+        {/* login form */}
+        <Image
+          src="/images/backgroundLogin.png"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
